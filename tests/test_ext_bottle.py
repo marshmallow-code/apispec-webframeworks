a1f6ee6c14196c2ec08e9ddec55e9b86153ad183
Marcin Lulek <info@webreactor.eu>
2018-10-07 14:19:16 +0200

    tests: move web framework tests from apispec

---
+++ tests/test_ext_bottle.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from bottle import route
+
+from apispec import APISpec
+from apispec_ext_webframeworks.bottle import BottlePlugin
+
+
+@pytest.fixture(params=('2.0', '3.0.0'))
+def spec(request):
+    return APISpec(
+        title='Swagger Petstore',
+        version='1.0.0',
+        openapi_version=request.param,
+        plugins=(BottlePlugin(), ),
+    )
+
+
+class TestPathHelpers:
+
+    def test_path_from_view(self, spec):
+        @route('/hello')
+        def hello():
+            return 'hi'
+        spec.add_path(
+            view=hello,
+            operations={'get': {'parameters': [], 'responses': {'200': {}}}},
+        )
+        assert '/hello' in spec._paths
+        assert 'get' in spec._paths['/hello']
+        expected = {'parameters': [], 'responses': {'200': {}}}
+        assert spec._paths['/hello']['get'] == expected
+
+    def test_path_with_multiple_methods(self, spec):
+
+        @route('/hello', methods=['GET', 'POST'])
+        def hello():
+            return 'hi'
+
+        spec.add_path(
+            view=hello, operations=dict(
+                get={'description': 'get a greeting', 'responses': {'200': {}}},
+                post={'description': 'post a greeting', 'responses': {'200': {}}},
+            ),
+        )
+        get_op = spec._paths['/hello']['get']
+        post_op = spec._paths['/hello']['post']
+        assert get_op['description'] == 'get a greeting'
+        assert post_op['description'] == 'post a greeting'
+
+    def test_integration_with_docstring_introspection(self, spec):
+
+        @route('/hello')
+        def hello():
+            """A greeting endpoint.
+
+            ---
+            x-extension: value
+            get:
+                description: get a greeting
+                responses:
+                    200:
+                        description: a pet to be returned
+                        schema:
+                            $ref: #/definitions/Pet
+
+            post:
+                description: post a greeting
+                responses:
+                    200:
+                        description: some data
+
+            foo:
+                description: not a valid operation
+                responses:
+                    200:
+                        description:
+                            more junk
+            """
+            return 'hi'
+
+        spec.add_path(view=hello)
+        get_op = spec._paths['/hello']['get']
+        post_op = spec._paths['/hello']['post']
+        extension = spec._paths['/hello']['x-extension']
+        assert get_op['description'] == 'get a greeting'
+        assert post_op['description'] == 'post a greeting'
+        assert 'foo' not in spec._paths['/hello']
+        assert extension == 'value'
+
+    def test_path_is_translated_to_openapi_template(self, spec):
+
+        @route('/pet/<pet_id>')
+        def get_pet(pet_id):
+            return 'representation of pet {pet_id}'.format(pet_id=pet_id)
+
+        spec.add_path(view=get_pet)
+        assert '/pet/{pet_id}' in spec._paths
+
+    def test_path_with_params(self, spec):
+
+        @route('/pet/<pet_id>/<shop_id>', methods=['POST'])
+        def set_pet():
+            return 'new pet!'
+
+        spec.add_path(view=set_pet)
+        assert '/pet/{pet_id}/{shop_id}' in spec._paths
---
+++ tests/test_ext_tornado.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from tornado.web import RequestHandler
+import tornado.gen
+
+from apispec import APISpec
+from apispec_ext_webframeworks.tornado import TornadoPlugin
+
+
+@pytest.fixture(params=('2.0', '3.0.0'))
+def spec(request):
+    return APISpec(
+        title='Swagger Petstore',
+        version='1.0.0',
+        openapi_version=request.param,
+        plugins=(TornadoPlugin(), ),
+    )
+
+
+class TestPathHelpers:
+
+    def test_path_from_urlspec(self, spec):
+        class HelloHandler(RequestHandler):
+            def get(self):
+                self.write('hello')
+
+        urlspec = (r'/hello', HelloHandler)
+        operations = (
+            {'get': {'parameters': [], 'responses': {'200': {}}}}
+        )
+
+        spec.add_path(urlspec=urlspec, operations=operations)
+        assert '/hello' in spec._paths
+        assert 'get' in spec._paths['/hello']
+        expected = {'parameters': [], 'responses': {'200': {}}}
+        assert spec._paths['/hello']['get'] == expected
+
+    def test_path_with_multiple_methods(self, spec):
+
+        class HelloHandler(RequestHandler):
+            def get(self):
+                self.write('hello')
+
+            def post(self):
+                self.write('hello')
+
+        urlspec = (r'/hello', HelloHandler)
+        operations = {
+            'get': {
+                'description': 'get a greeting',
+                'responses': {'200': {}},
+            },
+            'post': {
+                'description': 'post a greeting',
+                'responses': {'200': {}},
+            },
+        }
+        spec.add_path(urlspec=urlspec, operations=operations)
+        get_op = spec._paths['/hello']['get']
+        post_op = spec._paths['/hello']['post']
+        assert get_op['description'] == 'get a greeting'
+        assert post_op['description'] == 'post a greeting'
+
+    def test_integration_with_docstring_introspection(self, spec):
+
+        class HelloHandler(RequestHandler):
+            """
+            ---
+            x-extension: value
+            """
+            def get(self):
+                """Get a greeting endpoint.
+                ---
+                description: get a greeting
+                responses:
+                    200:
+                        description: a pet to be returned
+                        schema:
+                            $ref: #/definitions/Pet
+                """
+                self.write('hello')
+
+            def post(self):
+                """Post a greeting endpoint.
+                ---
+                description: post a greeting
+                responses:
+                    200:
+                        description: some data
+                """
+                self.write('hello')
+
+        urlspec = (r'/hello', HelloHandler)
+        spec.add_path(urlspec=urlspec)
+        get_op = spec._paths['/hello']['get']
+        post_op = spec._paths['/hello']['post']
+        extension = spec._paths['/hello']['x-extension']
+        assert get_op['description'] == 'get a greeting'
+        assert post_op['description'] == 'post a greeting'
+        assert extension == 'value'
+
+    def test_path_removing_trailing_or_optional_slash(self, spec):
+
+        class HelloHandler(RequestHandler):
+            def get(self):
+                self.write('hello world')
+
+        urlspec = (r'/hello/world/*', HelloHandler)
+        operations = (
+            {'get': {'parameters': [], 'responses': {'200': {}}}}
+        )
+
+        spec.add_path(urlspec=urlspec, operations=operations)
+        assert '/hello/world' in spec._paths
+
+    class HelloWorldHandler(RequestHandler):
+        def get(self, param1, param2):
+            self.write('hello')
+
+    class HelloWorldHandler2(RequestHandler):
+        @tornado.gen.coroutine
+        def get(self, param1, param2):
+            self.write('hello')
+
+    @pytest.mark.parametrize(
+        'Handler',
+        [
+            HelloWorldHandler,
+            HelloWorldHandler2,
+        ],
+    )
+    def test_path_with_params(self, spec, Handler):
+        urlspec = (r'/hello/([^/]+)/world/([^/]+)', Handler)
+        operations = (
+            {'get': {'parameters': [], 'responses': {'200': {}}}}
+        )
+
+        spec.add_path(urlspec=urlspec, operations=operations)
+        path = '/hello/{param1}/world/{param2}'
+        assert path in spec._paths
+        assert 'get' in spec._paths[path]
+        expected = {'parameters': [], 'responses': {'200': {}}}
+        assert spec._paths[path]['get'] == expected
