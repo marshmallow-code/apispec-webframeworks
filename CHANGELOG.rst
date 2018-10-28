ba0ef9d2f75bcddaab018d420a3e6652d81c5ba4
Steven Loria <sloria1@gmail.com>
2018-10-28 14:46:07 -0400

    Bump version and update changelog

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,7 +1,7 @@
 Changelog
 ---------
 
-0.1.0 (unreleased)
+0.1.0 (2018-10-28)
 ++++++++++++++++++
 
 * Test against Python 3.7.
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-VERSION = '0.0.2'
+VERSION = '0.1.0'
 EXTRAS_REQUIRE = {
     'tests': [
         'pytest',
