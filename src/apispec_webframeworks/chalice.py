dc597d31ea23c7b1e246ee115fd3da0653826400
Tim Reidel <tim.reidel@entrustdatacard.com>
2020-05-13 16:44:22 +0000

    re-do logic to make it more clear

--- src/apispec_webframeworks/chalice.py
+++ src/apispec_webframeworks/chalice.py
@@ -33,26 +33,36 @@ class ChalicePlugin(BasePlugin):
 
     @staticmethod
     def _route_for_view(app, operations, view):
-        # iterate through the routes and create a list of operations + routes for this view function
-        routes = {}
-        # iterate through all route entries
+        # iterate through the routes and look for a match to the view function 
+        route = None
+        # in the app objet for each path there's a dict with key=method and value=entry
+        # for each operation that we're iterested in see if there's an entry
         for path in app.routes.keys():
+            # get the map of method -> entry
             methods = app.routes[path]
+            # loop through the methodsoperations indicated in the specification 
             for method in methods.keys():
-                entry = methods[method]
-                if entry.view_function == view:
-                    routes[method] = path
-        # get the route
-        route = list(routes.values())[0]
-        # make sure all of the methods point to the same route for sanity checking
-        if list(routes.values()).count(route) != len(routes):
-            raise APISpecError(f"Method mismatch for route {view}")
+                # see if there's a match for the operation 
+                if len(operations) == 0 or method.lower() in operations:
+                    # there's an entry so see if the view methods match
+                    entry = methods[method]
+                    if entry.view_function == view:                        
+                        # store the path if this is the first match 
+                        if route is None:
+                             route = path
+                        # if the view natches a different path something is wrong
+                        elif route != path:
+                            raise APISpecError(f"Duplicate route found for method {view}")
+        # make sure we found one
+        if route is None:
+            raise APISpecError(f"No route found for method {view}")
+        # success 
         return route
 
     def path_helper(self, operations, *, view, app, **kwargs):
         """Path helper that allows passing a chalice view function."""
         if not isinstance(app, Chalice):
-            raise APISpecError(f"app must be an instance of chalice.Chalice")
+            raise APISpecError(f"app must be an instance of Chalice")
         # parse through the documentation string to see what operations are defined
         operations.update(yaml_utils.load_operations_from_docstring(view.__doc__))
         # find the route for this view function
